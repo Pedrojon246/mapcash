fe22f697c0414cec89dcec7f1323bb744e90c309
JonNorx Founder <joaonascimento156@gmail.com>
2026-08-21 15:21:59 -0300

    Update context.tsx

--- lib/i18n/context.tsx
+++ lib/i18n/context.tsx
@@ -28,7 +28,7 @@ export function I18nProvider({ children }: { children: ReactNode }) {
   }
 
   return (
-    <I18nContext.Provider value={{ locale, setLocale, t: translations[locale] }}>
+    <I18nContext.Provider value={{ locale, setLocale, t: translations[locale] as typeof translations.pt }}>
       {children}
     </I18nContext.Provider>
   )
